903e1fb83536299cd174c1b80c421dad3465f577
madebytimo <t.schnaible@madebytimo.de>
2022-11-13 13:06:27 +0100

    remove testing oputput

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,6 @@ mkdir -p /media/meshcentral/meshcentral-data
 mkdir -p /media/meshcentral/meshcentral-files
 ln --symbolic /media/meshcentral/meshcentral-data
 ln --symbolic /media/meshcentral/meshcentral-files
-ls
 
 cd /app/node_modules
 $@
