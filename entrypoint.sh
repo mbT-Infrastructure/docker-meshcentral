5058d7e0217d9a57bdd27f5644fd3921a58e2cd7
madebytimo <t.schnaible@madebytimo.de>
2022-11-13 13:41:12 +0100

    force symbolic links to overwrite existing

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@ set -e
 
 mkdir -p /media/meshcentral/meshcentral-data
 mkdir -p /media/meshcentral/meshcentral-files
-ln --symbolic /media/meshcentral/meshcentral-data
-ln --symbolic /media/meshcentral/meshcentral-files
+ln --symbolic --force /media/meshcentral/meshcentral-data
+ln --symbolic --force /media/meshcentral/meshcentral-files
 
 cd /app/node_modules
 $@
