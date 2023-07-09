b8a75fabbd122eb523063a84bfc2022beeaf3b97
madebytimo <t.schnaible@madebytimo.de>
2023-07-09 21:40:24 +0200

    make npm update silent

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,12 @@
 set -e
 
 cd /app
-npm update meshcentral
+npm update meshcentral --silent
 
 mkdir -p /media/meshcentral/meshcentral-data
 mkdir -p /media/meshcentral/meshcentral-files
-ln --symbolic --force /media/meshcentral/meshcentral-data
-ln --symbolic --force /media/meshcentral/meshcentral-files
+ln --symbolic --force /media/meshcentral/meshcentral-data .
+ln --symbolic --force /media/meshcentral/meshcentral-files .
 
 cd /app/node_modules
-$@
+"$@"
